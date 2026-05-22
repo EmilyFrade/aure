21bbf477894a25cca683fd716d7384933c0df380
Emily Frade <emilyfrade12@gmail.com>
2026-05-22 18:24:38 -0300

    gera a estrutura inicial do spring boot

---
+++ src/main/java/com/aure/AureApplication.java
@@ -0,0 +1,13 @@
+package com.aure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aure/AureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
